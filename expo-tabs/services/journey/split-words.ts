8b3e66489a7a21646acc260d7022c8e99d872251
Nick Etuk <nick.etuk1@nhs.net>
2023-01-07 19:50:36 +0000

    changed replaceAll

--- expo-tabs/services/journey/split-words.ts
+++ expo-tabs/services/journey/split-words.ts
@@ -2,8 +2,11 @@ import { WORD_BOUNDARIES } from "utils/constants";
 
 export function splitWords(text: string): string[] {
     let plainText = text.toLowerCase();
-    for (const char of WORD_BOUNDARIES)
-        plainText = plainText.replaceAll(char, " ");
+    for (const char of WORD_BOUNDARIES) {
+        // plainText = plainText.replaceAll(char, " ");
+        //plainText = plainText.replace(new RegExp(char, "g"), " ");
+        plainText = plainText.replace(char, " "); //todo: fix this.  proper replaceAll
+    }
 
     const words = plainText.split(" ");
     const distinctWords = [...new Set(words)];
